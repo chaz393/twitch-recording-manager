c296121385db0f2b81d6594b07e858c2fc7696c6
Patrick0071 <64305434+Patrick0071@users.noreply.github.com>
2023-12-24 17:19:48 +0100

    Added the --twitch-api-header option (#4)
    Merged PR from Patrick007 for adding twitch oauth token to remove ads from streamers you're subscribed to

--- start_recording.py
+++ start_recording.py
@@ -7,7 +7,6 @@ from RecordingThread import RecordingThread
 from datetime import datetime
 from config import Config
 
-
 def start():
     while True:
         try:
@@ -255,7 +254,7 @@ def start_recording_if_not_already(streams: dict):
             filename = f"{streamer_name}_TwitchVOD_{current_time}_{stream_title}.mp4"
             full_path = streamer_directory + filename
             create_streamer_folder_if_not_exists(streamer_directory)
-            start_recording(filename, full_path, streamer_name)
+            start_recording(filename, full_path, streamer_name, Config.TWITCH_OAUTH_TOKEN )
 
 
 def does_process_exist_for_streamer(streamer_name: str):
@@ -267,8 +266,8 @@ def create_streamer_folder_if_not_exists(streamer_directory: str):
         os.mkdir(streamer_directory)
 
 
-def start_recording(filename: str, full_path: str, streamer_name: str):
-    thread = RecordingThread(streamer_name, filename, full_path, recording_thread_finished_callback)
+def start_recording(filename: str, full_path: str, streamer_name: str, twitch_oauth_token):
+    thread = RecordingThread(streamer_name, filename, full_path, twitch_oauth_token, recording_thread_finished_callback)
     thread.start()
     recording_threads[streamer_name] = thread
 
@@ -289,6 +288,11 @@ def stop_recording_for_users_not_in_streamers_list(streamers: {}):
 recording_threads = {}
 access_token = ""
 access_token_expiration = 0
+
+if Config.TWITCH_OAUTH_TOKEN != "":
+    twitch_oauth_token = Config.TWITCH_OAUTH_TOKEN
+else:
+    Config.TWITCH_OAUTH_TOKEN = None
 
 if __name__ == "__main__":
     start()
