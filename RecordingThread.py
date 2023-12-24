e0cf31c96837fd59efe461da0dac9bf63b4b1e5d
clakinger <clakinger@gmail.com>
2023-12-24 11:37:40 -0600

    - clean up token usage - change argument format to match documentation

--- RecordingThread.py
+++ RecordingThread.py
@@ -29,6 +29,6 @@ class RecordingThread(threading.Thread):
     def start_recording(streamer_name: str, full_path: str, twitch_oauth_token: str):
         params = ["streamlink", "-o", full_path, f"https://www.twitch.tv/{streamer_name}", "best",
                   "--twitch-disable-hosting", "--twitch-disable-ads"]
-        if twitch_oauth_token is not "":
+        if twitch_oauth_token != "":
             params.extend([f"--twitch-api-header=\"Authorization=OAuth {twitch_oauth_token}\""])
         return subprocess.Popen(params)
